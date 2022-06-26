98ae2cecbe06cbec552c45de2e189564f44c53df
Bikram Ghimire <ikramghimire@gmil.com>
2022-06-26 20:11:59 +0545

    typescript lang lang

--- react-query/src/components/form/FormField.tsx
+++ react-query/src/components/form/FormField.tsx
@@ -1,13 +1,20 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
-import { useMutation } from "react-query";
+import { useMutation, useQueryClient } from "react-query";
 import { toast } from "react-toastify";
 import tutorialSevice from "../../services/TutorialService";
 // import Tutorial from "../types/Tutorial";
 
 const FormField: React.FC = () => {
+  const queryClient = useQueryClient();
   const mutation = useMutation((description: descType): any => {
-    tutorialSevice.create(description);
+    // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+    tutorialSevice.create(description),
+      {
+        onSuccess: () => {
+          queryClient.invalidateQueries("Alltutorial");
+        },
+      };
   });
   const [des, setDescription] = useState("");
 
